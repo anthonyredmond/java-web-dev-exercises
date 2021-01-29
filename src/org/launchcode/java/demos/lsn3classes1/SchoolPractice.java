1cf7c86acbf23da70461cede6b8742d61a49d97d
anthonyredmond <kushkurosaki@icloud.com>
2021-01-28 20:28:51 -0600

    finish part 1 of studio 7.9

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,6 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student anthony = new Student("anthony",1,1,4.0);
     }
 }
